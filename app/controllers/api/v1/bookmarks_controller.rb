39f28ae3203b299c966313938257c0aa0b8701a1
MatthewSerre <matthew.serre@gmail.com>
2019-10-18 18:55:50 -0700

    Add methods to bookmarks controller

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::BookmarksController < ApplicationController
+
+    def index
+        @bookmarks = Bookmark.all
+        render json: @bookmarks
+    end
+
+    def create
+        @bookmark = Bookmark.new(bookmark_params)
+        if @bookmark.save
+            render json: @bookmark
+        else
+            render json: {error: "Error creating bookmark"}
+        end
+    end
+
+    def show
+        @bookmark = Bookmark.find(:id)
+        render json: @bookmark
+    end
+
+    def destroy
+        @bookmark = Bookmark.find(:id)
+        @bookmark.destroy
+        render json: {notice: "Bookmark deleted"}
+    end
+
+    private
+
+    def bookmark_params
+        params.require(:bookmark).permit(:name, :url, :notes)
+    end
+
+
 end
