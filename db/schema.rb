4fd12cda1115d3c8b7fb9b8e8887461f59327a1b
Matthew Serre <matthew.serre@gmail.com>
2019-12-09 13:43:01 -0500

    Change database from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_19_011336) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
     t.integer "folder_id"
     t.string "name"
