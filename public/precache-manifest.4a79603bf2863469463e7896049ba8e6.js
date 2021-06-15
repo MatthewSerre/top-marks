bc08d1363aa5fb3cd573000d0d5621177c4aee87
Matthew Serre <matthew_serre+github@outlook.com>
2021-06-14 21:09:23 -0400

    Add yarn.lock to root

---
+++ public/precache-manifest.4a79603bf2863469463e7896049ba8e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9a061f6462e1bb3ab43e338d96d49c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55943b6727420cc4d8a5",
+    "url": "/static/js/2.9d71dc0e.chunk.js"
+  },
+  {
+    "revision": "2463ec0630d2fabe9e7e",
+    "url": "/static/js/main.da6f9f23.chunk.js"
+  },
+  {
+    "revision": "7c4c59398c541d182d23",
+    "url": "/static/js/runtime-main.7e35e11e.js"
+  }
+]);
